624a8b6280d683708c2b23dbdcc65e5310b0b05e
aleighbrown <brown.annaleigh@gmail.com>
2020-03-23 11:49:11 +0000

    Update converting_gene_lists.R
    fix this - had a bug

--- converting_gene_lists.R
+++ converting_gene_lists.R
@@ -14,27 +14,32 @@ convertGeneListSpecies <- function(genelist, from_species = "hsapiens_gene_ensem
   return(mapped_list)
 }
 #function coverts two different types of genes; using the annotation dbi
-convertGeneListTypes = function(genelist, from_type = "ENTREZID", to_type = "ENSEMBL", species = "mmusculus",print_keytpes = "FALSE"){
+convertGeneListTypes = function(genelist, from_type = "ENTREZID", to_type = "ENSEMBL", species = "mmusculus",print_keytypes = "FALSE"){
   #ensure you have
   if (!require("pacman")) install.packages("pacman")
   library(pacman)
   p_load(AnnotationDbi)
+  # if we just wanted to prrint key type to check
+  if(print_keytypes == TRUE){
+    print(keytypes(org.Mm.eg.db))
+    return()
+  }
+  # if we tried to type in a species that doesn't exists because I'm a bad speller
+  if(!species %in% c("mmusculus","hsapiens")){
+    print("Species must be 'mmusculus' or 'hsapiens'")
+  }
+  
+  
+  
   if( species == "mmusculus"){
-    anno_db = p_load(org.Mm.eg.db)
-    #sometimes I just wanna print the key types yo
-    if(print_keytpes == TRUE){
-      print(keytypes(org.Mm.eg.db))
-      return()
-    }else{
+    
+      anno_db = p_load(org.Mm.eg.db)
       mapped_list = as.data.table(AnnotationDbi::select(org.Mm.eg.db, keys =  unique(genelist), keytype = from_type, columns = c("SYMBOL",to_type)))
-    }
+  
   }else if(species == "hsapiens"){
     anno_db = p_load(org.Hs.eg.db)
     mapped_list = as.data.table(AnnotationDbi::select(org.Hs.eg.db, keys =  unique(genelist), keytype = from_type, columns = c("SYMBOL",to_type)))
-
-  }else{
-    return("species is currently either 'mmusculus' or 'hsapiens'")
-  }
+    }
 
   return(mapped_list)
 
