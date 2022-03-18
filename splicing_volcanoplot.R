55e2432c5dc9a6aa3bae753bf57745ac11c91e09
aleighbrown <brown.annaleigh@gmail.com>
2022-03-18 12:10:33 +0000

    Update splicing_volcanoplot.R

--- splicing_volcanoplot.R
+++ splicing_volcanoplot.R
@@ -1,17 +1,36 @@
-splicing_dots_tables_function <- function(input_splicing, list_gene) {
-  splicing_dots_tables <- input_splicing %>% 
-  mutate(junction_name = case_when(gene_name %in% list_gene &
-                                     probability_changing > 0.9  &
-                                     mean_dpsi_per_lsv_junction > 0 ~ gene_name,
-                                   T ~ "")) %>%
-  mutate(`Novel Junction` = de_novo_junctions == 1) %>%
-  mutate(log10_test_stat = -log10(1 - probability_changing)) %>%
-  mutate(log10_test_stat = ifelse(is.infinite(log10_test_stat), 4.5, log10_test_stat)) %>%
-  mutate(graph_alpha = ifelse(probability_changing > 0.9, 1, 0.2)) %>%
-  mutate(label_junction = case_when(gene_name %in% list_gene &
-                                      probability_changing > 0.9  &
-                                      mean_dpsi_per_lsv_junction > 0 ~ junction_name,
-                                    T ~ ""))
+splicing_dots_tables_function <- function(input_splicing, list_gene = "",psi_cutoff = 0,
+                                          probability_changing_cutoff = 0) {
+
+    if(psi_cutoff != 0 ){
+      psi_genes <- input_splicing %>% 
+        filter(mean_dpsi_per_lsv_junction >= psi_cutoff) %>% 
+        filter(probability_changing >= 0.9)
+    }
+  
+    if(probability_changing_cutoff != 0){
+      psi_genes = psi_genes %>% 
+        filter(probability_changing >= probability_changing_cutoff)
+    }
+    
+    if(exists('psi_genes')){
+
+      psi_genes = psi_genes %>% pull(gene_name)
+      list_gene = unique(c(list_gene,psi_genes))
+    }
+    splicing_dots_tables <- input_splicing %>% 
+      mutate(junction_name = case_when(gene_name %in% list_gene &
+                                         probability_changing > 0.9  &
+                                         mean_dpsi_per_lsv_junction > 0 ~ gene_name,
+                                       T ~ "")) %>%
+      mutate(`Novel Junction` = de_novo_junctions == 1) %>%
+      mutate(log10_test_stat = -log10(1 - probability_changing)) %>%
+      mutate(log10_test_stat = ifelse(is.infinite(log10_test_stat), 4.5, log10_test_stat)) %>%
+      mutate(graph_alpha = ifelse(probability_changing > 0.9, 1, 0.2)) %>%
+      mutate(label_junction = case_when(gene_name %in% list_gene &
+                                          probability_changing > 0.9  &
+                                          mean_dpsi_per_lsv_junction > 0 ~ junction_name,
+                                        T ~ ""))
+
 
 fig1 <- ggplot() +
   geom_point(data = splicing_dots_tables %>% dplyr::filter(de_novo_junctions == 0),
